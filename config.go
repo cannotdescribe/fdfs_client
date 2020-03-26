5034f2629956c702749a6a0e6b1039e8941063af
cannotdescribe <952376923@qq.com>
2020-03-26 09:18:40 +0800

    go mod 补充修改

--- config.go
+++ config.go
@@ -31,24 +31,27 @@ func newConfig(configName string) (*config, error) {
 	reader := bufio.NewReader(f)
 	for {
 		line, err := reader.ReadString('\n')
+
+		line = strings.TrimSuffix(line, splitFlag)
+		str := strings.SplitN(line, "=", 2)
+		if len(str) >= 2 {
+			key := strings.TrimSpace(str[0])
+			value := strings.TrimSpace(str[1])
+			switch key {
+			case "tracker_server":
+				config.trackerAddr = append(config.trackerAddr, value)
+			case "maxConns":
+				config.maxConns, err = strconv.Atoi(value)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
 		if err != nil {
 			if err == io.EOF {
 				return config, nil
 			}
 			return nil, err
-		}
-		line = strings.TrimSuffix(line, splitFlag)
-		str := strings.SplitN(line, "=", 2)
-		key := strings.TrimSpace(str[0])
-		value := strings.TrimSpace(str[1])
-		switch key {
-		case "tracker_server":
-			config.trackerAddr = append(config.trackerAddr, value)
-		case "maxConns":
-			config.maxConns, err = strconv.Atoi(value)
-			if err != nil {
-				return nil, err
-			}
 		}
 	}
 	return config, nil
